843659e47162ada237085b983a9a60b2a87dce11
Jon Calanio <joncalanio@gmail.com>
2017-10-11 16:50:05 -0500

    [master] Adds basic objects and imports the Forms Module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 
+//Custom Data Objects
+export class SampleObject {
+  title: string;
+  desc: string;
+  num: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  //Static Messages
   title = 'VWR';
   message = 'This is an RSS app designed to help learn Angular.'
+
+  //Object Exports
+  object1: SampleObject = {
+    title: 'Object 1',
+    desc: 'This is the first object.',
+    num: 442
+  };
+
+  object2: SampleObject = {
+    title: 'Object 2',
+    desc: 'This is the second object. Test.',
+    num: 5335
+  };
 }
