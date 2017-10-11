1062588770d203033ee3f6f0ddb0dc5580c9e3d0
Jon Calanio <joncalanio@gmail.com>
2017-10-11 11:15:34 -0500

    [master] Initial changes just to test out Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'VWR';
+  message = 'This is an RSS app designed to help learn Angular.'
 }
