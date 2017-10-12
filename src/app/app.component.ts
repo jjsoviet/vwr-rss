af21c98a85b736d57a6ba68c2d881474c2f36ca5
jjsoviet <joncalanio@gmail.com>
2017-10-12 11:04:41 -0500

    [prototype] Adds the Prototype branch for learning the basics of Angular. Now includes a test list of objects.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,14 @@ export class SampleObject {
   num: number;
 }
 
+const OBJECTS: SampleObject[] = [
+  { title: 'object 1', desc: 'this is object 1', num: 3342 },
+  { title: 'object 2', desc: 'this is object 2', num: 4553 },
+  { title: 'object 3', desc: 'this is object 3', num: 8643 },
+  { title: 'object 4', desc: 'this is object 4', num: 2249 },
+  { title: 'object 5', desc: 'this is object 5', num: 8954 }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,7 +24,8 @@ export class SampleObject {
 export class AppComponent {
   //Static Messages
   title = 'VWR';
-  message = 'This is an RSS app designed to help learn Angular.'
+  message = 'This is an RSS app designed to help learn Angular.';
+  objects = OBJECTS;
 
   //Object Exports
   object1: SampleObject = {
