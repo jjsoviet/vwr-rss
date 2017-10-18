11abcd45a610fec838a07bbcabc3a4a6f4612ae8
Jon Calanio <joncalanio@gmail.com>
2017-10-18 14:52:58 -0500

    [prototype] Forgot to remove hard-coded RSS URL in the Feed service, now uses the URL instantiated in the feed view component.

--- src/app/services/feed.service.ts
+++ src/app/services/feed.service.ts
@@ -17,8 +17,8 @@ export class FeedService {
 
   //Functions
   initializeSource(source: string): void {
-    this.sourceURL = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Ffeeds.bbci.co.uk%2Fnews%2Frss.xml%3Fedition%3Dus';
-    console.log(this.sourceURL);
+    this.sourceURL = source;
+    console.log("Source: " + this.sourceURL);
   }
 
   //Return the RSS Feed information
